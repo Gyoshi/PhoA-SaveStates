bbb3b5eb1b8915084c5182851511912625c73473
Johannes vd Berg <bakafish.com@gmail.com>
2023-02-13 23:19:42 +0100

    Fix camera and encounter position bugs

--- Main.cs
+++ Main.cs
@@ -5,17 +5,19 @@ using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityModManagerNet;
+using static UnityEngine.Random;
 
 namespace SaveStates
 {
-    #if DEBUG
+#if DEBUG
     [EnableReloading]
-    #endif
+#endif
     static class Main
     {
         [SaveOnReload] public static string room;
         [SaveOnReload] public static Vector3 position;
-        [SaveOnReload] public static GALE_MODE mode;
+        [SaveOnReload] public static Vector3 encounterPosition;
+        [SaveOnReload] public static int camera;
         [SaveOnReload] public static GALE_MODE loadMode;
 
         public static Harmony harmony;
@@ -23,6 +25,7 @@ namespace SaveStates
         // Compiled without dependencies on UnityModManagerNet
         static void Load(UnityModManager.ModEntry modEntry)
         {
+            // TODO: figure this out (easy fix if camera disabled?)
             //modEntry.OnUpdate = OnUpdate;
             modEntry.OnLateUpdate = OnUpdate;
             #if DEBUG
@@ -62,22 +65,38 @@ namespace SaveStates
 
                 // Save position
                 position = PT2.gale_interacter.GetGaleTransform().position;
-                modEntry.Logger.Log("saved position : " + position);
+                encounterPosition = new Vector3(WorldMapFoeLogic.X_WHERE_BATTLE_OCCURRED, WorldMapFoeLogic.Y_WHERE_BATTLE_OCCURRED, 0f);
+                //checkpoint = { PT2.gale_interacter._checkpoint_location, ...}
+                modEntry.Logger.Log("Saved position : " + position);
 
-                //mode = ???;
-                //loadMode = Traverse.Create(typeof(GaleLogicOne)).Field("_gale_state_on_level_load").GetValue<GALE_MODE>(PT2.gale_script);
+                // Save camera
+                camera = PT2.camera_control._curr_camera_config;
+
+                // Saving Gail's exact state (rolling, dying, climbing, etc.) is probly not worth the effort
+                //galeState = Traverse.Create(typeof(GaleLogicOne)).Field("StateFn").GetValue<Action>(PT2.gale_script);
+                // Save more general mode
+                FieldInfo field = typeof(GaleLogicOne).GetField("_gale_state_on_level_load", BindingFlags.NonPublic | BindingFlags.Instance);
+                loadMode = (GALE_MODE)field.GetValue(PT2.gale_script);
+                modEntry.Logger.Log("Saved load mode : " + loadMode);
+
             }
             if (PT2.director.control.GRAB_HELD && PT2.director.control.CAM_PRESSED)
             {
-                //PT2.gale_script.SetGaleModeOnLevelLoad(mode);
-                //PT2.gale_script.SendGaleCommand(GALE_CMD.SET_GALE_MODE);
-                //PT2.gale_script.SetGaleModeOnLevelLoad(loadMode);
 
                 PT2.LoadLevel(room, 0, Vector3.zero, false, 0.1f, false, true);
+                PT2.gale_script.SendGaleCommand(GALE_CMD.RESET);
+                PT2.gale_script.SetGaleModeOnLevelLoad(loadMode);
+                PT2.gale_script.SendGaleCommand(GALE_CMD.SET_GALE_MODE);
+
                 PT2.gale_interacter.GetGaleTransform().position = position;
+                WorldMapFoeLogic.X_WHERE_BATTLE_OCCURRED = encounterPosition.x;
+                WorldMapFoeLogic.Y_WHERE_BATTLE_OCCURRED = encounterPosition.y;
+
+                PT2.camera_control.SwitchCameraConfig(camera, 0, true);
                 OpeningMenuLogic.EnableGameplayElements();
 
                 PT2.gale_interacter.DisplayNumAboveHead(10, DamageNumberLogic.DISPLAY_STYLE.HOVER_AND_FLASH_GREEN, true);
+                //TODO: add nice sound effects
                 modEntry.Logger.Log("ロード済み");
             }
         }
